9442b5c490c5928536ae5501b8c2205e05d289e0
xin <1195059682@qq.com>
2023-03-09 15:24:14 +0800

    集成flowable

---
+++ jlkj-modules/jlkj-flowable/src/main/java/com/jlkj/flowable/FlowableApplication.java
@@ -0,0 +1,22 @@
+package com.jlkj.flowable;
+
+import com.jlkj.common.security.annotation.EnableCustomConfig;
+import com.jlkj.common.security.annotation.EnableRyFeignClients;
+import com.jlkj.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xin
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class FlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  工作流模块启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
