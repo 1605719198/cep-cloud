81da8fda356beef03a2642d301ae818049d8f1e9
111191 <xiaobo@ejianlong.com>
2023-03-21 10:04:30 +0800

    重命名文件，以符合开发规范

---
+++ jlkj-modules/jlkj-human/src/main/java/com/jlkj/human/config/PinYinApi.java
@@ -0,0 +1,64 @@
+package com.jlkj.human.config;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+
+/**
+ * @author 111191
+ * @version 1.0
+ * @className PinYinApi
+ * @description TODO
+ * @date 2023年3月21日, 0021 10
+ */
+public class PinYinApi {
+
+    /**
+     * 提取每个汉字的首字母(大写)
+     *
+     * @param str
+     * @return
+     */
+    public static String getPinYinHeadChar(String str) {
+        if (isNull(str)) {
+            return "";
+        }
+        String convert = "";
+        for (int j = 0; j < str.length(); j++) {
+            char word = str.charAt(j);
+            // 提取汉字的首字母
+            String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+            if (pinyinArray != null) {
+                convert += pinyinArray[0].charAt(0);
+            }
+            else {
+                convert += word;
+            }
+        }
+
+        convert = string2AllTrim(convert);
+        return convert.toUpperCase();
+    }
+
+    /*
+     * 判断字符串是否为空
+     */
+
+    public static boolean isNull(Object strData) {
+        if (strData == null || "".equals(String.valueOf(strData).trim())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 去掉字符串包含的所有空格
+     *
+     * @param value
+     * @return
+     */
+    public static String string2AllTrim(String value) {
+        if (isNull(value)) {
+            return "";
+        }
+        return value.trim().replace(" ", "");
+    }
+}
