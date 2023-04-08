945a6d2f24710e48306ac33e016ec3ea9f0e5656
wangmai <1481070853@qq.com>
2023-04-08 10:59:56 +0800

    往来公司查询后端

---
+++ jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/gp/mapper/FinanceGpManufacturerBaseMapper.java
@@ -0,0 +1,68 @@
+package com.jlkj.finance.gp.mapper;
+
+import java.util.List;
+import com.jlkj.finance.gp.domain.FinanceGpManufacturerBase;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 销售管理-厂商基本资料Mapper接口
+ * 
+ * @author 265799
+ * @date 2023-04-08
+ */
+public interface FinanceGpManufacturerBaseMapper 
+{
+    /**
+     * 查询销售管理-厂商基本资料
+     * 
+     * @param id 销售管理-厂商基本资料主键
+     * @return 销售管理-厂商基本资料
+     */
+    public FinanceGpManufacturerBase selectFinanceGpManufacturerBaseById(String id);
+
+    /**
+     * 查询销售管理-厂商基本资料列表
+     *
+     * @param mainAreaId
+     * @param detailAreaId
+     * @param originalManufacturerChineseName
+     * @param startDate
+     * @param endDate
+     * @return 销售管理-厂商基本资料集合
+     */
+    public List<FinanceGpManufacturerBase> selectFinanceGpManufacturerBaseList(@Param("mainAreaId")String mainAreaId, @Param("detailAreaId")String detailAreaId,
+                                                                               @Param("originalManufacturerChineseName")   String originalManufacturerChineseName,
+                                                                               @Param("startDate") String startDate, @Param("endDate")  String endDate);
+
+    /**
+     * 新增销售管理-厂商基本资料
+     * 
+     * @param financeGpManufacturerBase 销售管理-厂商基本资料
+     * @return 结果
+     */
+    public int insertFinanceGpManufacturerBase(FinanceGpManufacturerBase financeGpManufacturerBase);
+
+    /**
+     * 修改销售管理-厂商基本资料
+     * 
+     * @param financeGpManufacturerBase 销售管理-厂商基本资料
+     * @return 结果
+     */
+    public int updateFinanceGpManufacturerBase(FinanceGpManufacturerBase financeGpManufacturerBase);
+
+    /**
+     * 删除销售管理-厂商基本资料
+     * 
+     * @param id 销售管理-厂商基本资料主键
+     * @return 结果
+     */
+    public int deleteFinanceGpManufacturerBaseById(String id);
+
+    /**
+     * 批量删除销售管理-厂商基本资料
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteFinanceGpManufacturerBaseByIds(String[] ids);
+}
