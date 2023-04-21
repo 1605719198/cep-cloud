729b534c726b07b5abe6a5cb6bdc398cb74062f1
zhangyouxin <zhangyouxin@ejianlong.com>
2023-04-21 09:42:30 +0800

    会计科目-集团级

---
+++ jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/aa/mapper/FinanceAcctcodeGroupMapper.java
@@ -0,0 +1,85 @@
+package com.jlkj.finance.aa.mapper;
+
+import java.util.List;
+import com.jlkj.finance.aa.domain.FinanceAcctcodeGroup;
+
+/**
+ * 会计科目-集团级Mapper接口
+ *
+ * @author 116514
+ * @date 2023-04-17
+ */
+public interface FinanceAcctcodeGroupMapper
+{
+    /**
+     * 查询会计科目-集团级
+     *
+     * @param groupAcctId 会计科目-集团级主键
+     * @return 会计科目-集团级
+     */
+    public FinanceAcctcodeGroup selectFinanceAcctcodeGroupByGroupAcctId(String groupAcctId);
+
+    /**
+     * 查询会计科目-集团级列表
+     *
+     * @param financeAcctcodeGroup 会计科目-集团级
+     * @return 会计科目-集团级集合
+     */
+    public List<FinanceAcctcodeGroup> selectFinanceAcctcodeGroupList(FinanceAcctcodeGroup financeAcctcodeGroup);
+
+    /**
+     * 新增会计科目-集团级
+     *
+     * @param financeAcctcodeGroup 会计科目-集团级
+     * @return 结果
+     */
+    public int insertFinanceAcctcodeGroup(FinanceAcctcodeGroup financeAcctcodeGroup);
+
+    /**
+     * 修改会计科目-集团级
+     *
+     * @param financeAcctcodeGroup 会计科目-集团级
+     * @return 结果
+     */
+    public int updateFinanceAcctcodeGroup(FinanceAcctcodeGroup financeAcctcodeGroup);
+
+    /**
+     * 删除会计科目-集团级
+     *
+     * @param groupAcctId 会计科目-集团级主键
+     * @return 结果
+     */
+    public int deleteFinanceAcctcodeGroupByGroupAcctId(String groupAcctId);
+
+    /**
+     * 批量删除会计科目-集团级
+     *
+     * @param groupAcctIds 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteFinanceAcctcodeGroupByGroupAcctIds(String[] groupAcctIds);
+
+    /**
+     * 查询会计科目-集团级
+     * @param GroupAcctCode
+     * @return
+     */
+    public FinanceAcctcodeGroup selectFinanceAcctcodeGroupByGroupAcctCode(String GroupAcctCode);
+
+    /**
+     * 查询会计科目子节点
+     * @param GroupAcctCode
+     * @return
+     */
+    public List<FinanceAcctcodeGroup> selectFinancAcctcodeChildrenList(String GroupAcctCode);
+
+    /**
+     * 通过GroupAcctCode删除会计科目
+     * @param GroupAcctCode
+     * @return
+     */
+    public int deleteFinanceAcctcodeGroupByGroupAcctCode(String GroupAcctCode);
+
+
+    public List<FinanceAcctcodeGroup> selectCodeGroupList(String acctCode);
+}
