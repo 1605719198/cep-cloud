976584560f79843e47d68f33aebbb21f353827aa
zhangyouxin <zhangyouxin@ejianlong.com>
2023-04-21 09:44:06 +0800

    会计科目-公司级domain

---
+++ jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/aa/domain/FinanceAaAcctcodeCorp.java
@@ -0,0 +1,428 @@
+package com.jlkj.finance.aa.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.jlkj.common.core.annotation.Excel;
+import com.jlkj.common.core.web.domain.BaseEntity;
+
+/**
+ * 会计科目-公司级对象 finance_aa_acctcode
+ *
+ * @author 116514
+ * @date 2023-04-19
+ */
+public class FinanceAaAcctcodeCorp extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 会计科目主键 */
+    @Excel(name = "会计科目主键")
+    private String acctId;
+
+    /** 会计公司id */
+    @Excel(name = "会计公司id")
+    private String companyId;
+
+    /** 集团会计科目主键 */
+    private String groupAcctId;
+
+    /** 会计科目编号 */
+    @Excel(name = "会计科目编号")
+    private String acctCode;
+
+    /** 会计科目中文名称 */
+    @Excel(name = "会计科目中文名称")
+    private String acctName;
+
+    /** 上层会计科目ID */
+    private String parentId;
+
+    /** 上层会计科目编号 */
+    private String parentAcctCode;
+
+    /** 传票性科目 */
+    @Excel(name = "传票性科目")
+    private String isVoucher;
+
+    /** 借/贷方会计科目 */
+    private String drOrCr;
+
+    /** 现金科目 */
+    private String isCash;
+
+    /** 级别 */
+    private String level;
+
+    /** 祖级列表 */
+    private String ancestors;
+
+    /** 数量金额辅助核算（单位） */
+    @Excel(name = "数量金额辅助核算", readConverterExp = "单=位")
+    private String isUnit;
+
+    /** 核算项目一id */
+    private String calTypeIda;
+
+    /** 核算项目一编号 */
+    @Excel(name = "核算项目一编号")
+    private String calTypeCodea;
+
+    /** 核算项目二id */
+    private String calTypeIdb;
+
+    /** 核算项目二编号 */
+    @Excel(name = "核算项目二编号")
+    private String calTypeCodeb;
+
+    /** 核算项目三id */
+    private String calTypeIdc;
+
+    /** 核算项目三编号 */
+    @Excel(name = "核算项目三编号")
+    private String calTypeCodec;
+
+    /** 核算项目四id */
+    private String calTypeIdd;
+
+    /** 核算项目四编号 */
+    @Excel(name = "核算项目四编号")
+    private String calTypeCoded;
+
+    /** 到期日 */
+    private String isDueDate;
+
+    /** 悬计帐 */
+    private String isLqdt;
+
+    /** 外币核算（币别） */
+    private String isFrnCrcy;
+
+    /** 机器专用 */
+    private String isPrvlg;
+
+    /** 禁用标识 */
+    @Excel(name = "禁用标识")
+    private String disabledCode;
+
+    /** 禁用日期 */
+    private Date disabledDate;
+
+    /** 新增人名称 */
+    private String createName;
+
+    /** 异动人名称 */
+    private String updateName;
+
+    /** 子节点 */
+    private List<FinanceAaAcctcodeCorp> children = new ArrayList<FinanceAaAcctcodeCorp>();
+
+    public void setAcctId(String acctId)
+    {
+        this.acctId = acctId;
+    }
+
+    public String getAcctId()
+    {
+        return acctId;
+    }
+    public void setCompanyId(String companyId)
+    {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyId()
+    {
+        return companyId;
+    }
+    public void setGroupAcctId(String groupAcctId)
+    {
+        this.groupAcctId = groupAcctId;
+    }
+
+    public String getGroupAcctId()
+    {
+        return groupAcctId;
+    }
+    public void setAcctCode(String acctCode)
+    {
+        this.acctCode = acctCode;
+    }
+
+    public String getAcctCode()
+    {
+        return acctCode;
+    }
+    public void setAcctName(String acctName)
+    {
+        this.acctName = acctName;
+    }
+
+    public String getAcctName()
+    {
+        return acctName;
+    }
+    public void setParentId(String parentId)
+    {
+        this.parentId = parentId;
+    }
+
+    public String getParentId()
+    {
+        return parentId;
+    }
+    public void setParentAcctCode(String parentAcctCode)
+    {
+        this.parentAcctCode = parentAcctCode;
+    }
+
+    public String getParentAcctCode()
+    {
+        return parentAcctCode;
+    }
+    public void setIsVoucher(String isVoucher)
+    {
+        this.isVoucher = isVoucher;
+    }
+
+    public String getIsVoucher()
+    {
+        return isVoucher;
+    }
+    public void setDrOrCr(String drOrCr)
+    {
+        this.drOrCr = drOrCr;
+    }
+
+    public String getDrOrCr()
+    {
+        return drOrCr;
+    }
+    public void setIsCash(String isCash)
+    {
+        this.isCash = isCash;
+    }
+
+    public String getIsCash()
+    {
+        return isCash;
+    }
+    public void setLevel(String level)
+    {
+        this.level = level;
+    }
+
+    public String getLevel()
+    {
+        return level;
+    }
+    public void setAncestors(String ancestors)
+    {
+        this.ancestors = ancestors;
+    }
+
+    public String getAncestors()
+    {
+        return ancestors;
+    }
+    public void setIsUnit(String isUnit)
+    {
+        this.isUnit = isUnit;
+    }
+
+    public String getIsUnit()
+    {
+        return isUnit;
+    }
+    public void setCalTypeIda(String calTypeIda)
+    {
+        this.calTypeIda = calTypeIda;
+    }
+
+    public String getCalTypeIda()
+    {
+        return calTypeIda;
+    }
+    public void setCalTypeCodea(String calTypeCodea)
+    {
+        this.calTypeCodea = calTypeCodea;
+    }
+
+    public String getCalTypeCodea()
+    {
+        return calTypeCodea;
+    }
+    public void setCalTypeIdb(String calTypeIdb)
+    {
+        this.calTypeIdb = calTypeIdb;
+    }
+
+    public String getCalTypeIdb()
+    {
+        return calTypeIdb;
+    }
+    public void setCalTypeCodeb(String calTypeCodeb)
+    {
+        this.calTypeCodeb = calTypeCodeb;
+    }
+
+    public String getCalTypeCodeb()
+    {
+        return calTypeCodeb;
+    }
+    public void setCalTypeIdc(String calTypeIdc)
+    {
+        this.calTypeIdc = calTypeIdc;
+    }
+
+    public String getCalTypeIdc()
+    {
+        return calTypeIdc;
+    }
+    public void setCalTypeCodec(String calTypeCodec)
+    {
+        this.calTypeCodec = calTypeCodec;
+    }
+
+    public String getCalTypeCodec()
+    {
+        return calTypeCodec;
+    }
+    public void setCalTypeIdd(String calTypeIdd)
+    {
+        this.calTypeIdd = calTypeIdd;
+    }
+
+    public String getCalTypeIdd()
+    {
+        return calTypeIdd;
+    }
+    public void setCalTypeCoded(String calTypeCoded)
+    {
+        this.calTypeCoded = calTypeCoded;
+    }
+
+    public String getCalTypeCoded()
+    {
+        return calTypeCoded;
+    }
+    public void setIsDueDate(String isDueDate)
+    {
+        this.isDueDate = isDueDate;
+    }
+
+    public String getIsDueDate()
+    {
+        return isDueDate;
+    }
+    public void setIsLqdt(String isLqdt)
+    {
+        this.isLqdt = isLqdt;
+    }
+
+    public String getIsLqdt()
+    {
+        return isLqdt;
+    }
+    public void setIsFrnCrcy(String isFrnCrcy)
+    {
+        this.isFrnCrcy = isFrnCrcy;
+    }
+
+    public String getIsFrnCrcy()
+    {
+        return isFrnCrcy;
+    }
+    public void setIsPrvlg(String isPrvlg)
+    {
+        this.isPrvlg = isPrvlg;
+    }
+
+    public String getIsPrvlg()
+    {
+        return isPrvlg;
+    }
+    public void setDisabledCode(String disabledCode)
+    {
+        this.disabledCode = disabledCode;
+    }
+
+    public String getDisabledCode()
+    {
+        return disabledCode;
+    }
+    public void setDisabledDate(Date disabledDate)
+    {
+        this.disabledDate = disabledDate;
+    }
+
+    public Date getDisabledDate()
+    {
+        return disabledDate;
+    }
+    public void setCreateName(String createName)
+    {
+        this.createName = createName;
+    }
+
+    public String getCreateName()
+    {
+        return createName;
+    }
+    public void setUpdateName(String updateName)
+    {
+        this.updateName = updateName;
+    }
+
+    public String getUpdateName()
+    {
+        return updateName;
+    }
+
+    public List<FinanceAaAcctcodeCorp> getChildren() { return children; }
+
+    public void setChildren(List<FinanceAaAcctcodeCorp> children) { this.children = children; }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("acctId", getAcctId())
+            .append("companyId", getCompanyId())
+            .append("groupAcctId", getGroupAcctId())
+            .append("acctCode", getAcctCode())
+            .append("acctName", getAcctName())
+            .append("parentId", getParentId())
+            .append("parentAcctCode", getParentAcctCode())
+            .append("isVoucher", getIsVoucher())
+            .append("drOrCr", getDrOrCr())
+            .append("isCash", getIsCash())
+            .append("level", getLevel())
+            .append("ancestors", getAncestors())
+            .append("isUnit", getIsUnit())
+            .append("calTypeIda", getCalTypeIda())
+            .append("calTypeCodea", getCalTypeCodea())
+            .append("calTypeIdb", getCalTypeIdb())
+            .append("calTypeCodeb", getCalTypeCodeb())
+            .append("calTypeIdc", getCalTypeIdc())
+            .append("calTypeCodec", getCalTypeCodec())
+            .append("calTypeIdd", getCalTypeIdd())
+            .append("calTypeCoded", getCalTypeCoded())
+            .append("isDueDate", getIsDueDate())
+            .append("isLqdt", getIsLqdt())
+            .append("isFrnCrcy", getIsFrnCrcy())
+            .append("isPrvlg", getIsPrvlg())
+            .append("disabledCode", getDisabledCode())
+            .append("disabledDate", getDisabledDate())
+            .append("createBy", getCreateBy())
+            .append("createName", getCreateName())
+            .append("createTime", getCreateTime())
+            .append("updateBy", getUpdateBy())
+            .append("updateName", getUpdateName())
+            .append("updateTime", getUpdateTime())
+            .toString();
+    }
+}
