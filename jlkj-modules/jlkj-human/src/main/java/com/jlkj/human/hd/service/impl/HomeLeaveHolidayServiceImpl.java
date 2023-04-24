30ff43f41de9b652af54fa7b1bf146a755fa5388
jiangbingchen <114288>
2023-04-24 15:52:59 +0800

    人事二期探亲假作业

--- jlkj-modules/jlkj-human/src/main/java/com/jlkj/human/hd/service/impl/HomeLeaveHolidayServiceImpl.java
+++ jlkj-modules/jlkj-human/src/main/java/com/jlkj/human/hd/service/impl/HomeLeaveHolidayServiceImpl.java
@@ -1,11 +1,18 @@
 package com.jlkj.human.hd.service.impl;
 
+import com.jlkj.common.core.exception.ServiceException;
+import com.jlkj.common.core.utils.StringUtils;
+import com.jlkj.common.core.utils.bean.BeanValidators;
+import com.jlkj.common.core.utils.uuid.IdUtils;
+import com.jlkj.common.security.utils.SecurityUtils;
 import com.jlkj.human.hd.domain.HomeLeaveHoliday;
 import com.jlkj.human.hd.mapper.HomeLeaveHolidayMapper;
 import com.jlkj.human.hd.service.IHomeLeaveHolidayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Validator;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -20,6 +27,9 @@ public class HomeLeaveHolidayServiceImpl implements IHomeLeaveHolidayService
     @Autowired
     private HomeLeaveHolidayMapper homeLeaveHolidayMapper;
 
+    @Autowired
+    protected Validator validator;
+
     /**
      * 查询探亲假天数设定
      *
@@ -30,6 +40,18 @@ public class HomeLeaveHolidayServiceImpl implements IHomeLeaveHolidayService
     public HomeLeaveHoliday selectHomeLeaveHolidayById(String id)
     {
         return homeLeaveHolidayMapper.selectHomeLeaveHolidayById(id);
+    }
+
+    /**
+     * 查询探亲假天数设定
+     *
+     * @param homeLeaveHoliday 探亲假天数设定主键
+     * @return 探亲假天数设定
+     */
+    @Override
+    public HomeLeaveHoliday selectHomeLeaveHolidayByempNo(HomeLeaveHoliday homeLeaveHoliday)
+    {
+        return homeLeaveHolidayMapper.selectHomeLeaveHolidayByempNo(homeLeaveHoliday);
     }
 
     /**
@@ -53,6 +75,8 @@ public class HomeLeaveHolidayServiceImpl implements IHomeLeaveHolidayService
     @Override
     public int insertHomeLeaveHoliday(HomeLeaveHoliday homeLeaveHoliday)
     {
+        homeLeaveHoliday.setId(IdUtils.fastSimpleUUID());
+        homeLeaveHoliday.setCreator(SecurityUtils.getUsername());
         return homeLeaveHolidayMapper.insertHomeLeaveHoliday(homeLeaveHoliday);
     }
 
@@ -90,5 +114,70 @@ public class HomeLeaveHolidayServiceImpl implements IHomeLeaveHolidayService
     public int deleteHomeLeaveHolidayById(String id)
     {
         return homeLeaveHolidayMapper.deleteHomeLeaveHolidayById(id);
+    }
+
+    /**
+     * 导入探亲假数据
+     *
+     * @param homeLeaveHolidayList 用户探亲假列表
+     * @param isUpdateSupport 是否更新支持，如果已存在，则进行更新数据
+     * @param operName 操作用户
+     * @return 结果
+     */
+    @Override
+    public String importUser(List<HomeLeaveHoliday> homeLeaveHolidayList, Boolean isUpdateSupport, String operName)
+    {
+        if (StringUtils.isNull(homeLeaveHolidayList) || homeLeaveHolidayList.size() == 0)
+        {
+            throw new ServiceException("导入探亲假数据不能为空！");
+        }
+        int successNum = 0;
+        int failureNum = 0;
+        StringBuilder successMsg = new StringBuilder();
+        StringBuilder failureMsg = new StringBuilder();
+        for (HomeLeaveHoliday homeLeaveHoliday : homeLeaveHolidayList)
+        {
+            try
+            {
+                // 查询该员工是否有剩余年休假，如果有则进行清空，重新赋值可休天数，已休天数，剩余可休天数
+                //
+                HomeLeaveHoliday u = homeLeaveHolidayMapper.selectHomeLeaveHolidayByEmpNo(homeLeaveHoliday);
+                if (!u.getPreHomeDays().toString().equals("0")) {
+                    homeLeaveHoliday.setDueHomeDays(BigDecimal.valueOf(0));
+                    homeLeaveHoliday.setRestHomeDays(BigDecimal.valueOf(0));
+                    homeLeaveHoliday.setPreHomeDays(BigDecimal.valueOf(0));
+                }
+                    BeanValidators.validateWithException(validator, homeLeaveHoliday);
+                    homeLeaveHoliday.setCreator(operName);
+                    homeLeaveHolidayMapper.updateHomeLeaveHolidayByEmpNo(homeLeaveHoliday);
+                    successNum++;
+                    successMsg.append("<br/>" + successNum + "、工号 " + homeLeaveHoliday.getEmpNo() + " 导入成功");
+                if (isUpdateSupport)
+                {
+                    BeanValidators.validateWithException(validator, homeLeaveHoliday);
+                    homeLeaveHoliday.setCreator(operName);
+                    //
+                    homeLeaveHolidayMapper.updateHomeLeaveHoliday(homeLeaveHoliday);
+                    successNum++;
+                    successMsg.append("<br/>" + successNum + "、工号 " + homeLeaveHoliday.getEmpNo() + " 更新成功");
+                }
+            }
+            catch (Exception e)
+            {
+                failureNum++;
+                String msg = "<br/>" + failureNum + "、工号 " + homeLeaveHoliday.getEmpNo() + " 导入失败：";
+                failureMsg.append(msg + e.getMessage());
+            }
+        }
+        if (failureNum > 0)
+        {
+            failureMsg.insert(0, "很抱歉，导入失败！共 " + failureNum + " 条数据格式不正确，错误如下：");
+            throw new ServiceException(failureMsg.toString());
+        }
+        else
+        {
+            successMsg.insert(0, "恭喜您，数据已全部导入成功！共 " + successNum + " 条，数据如下：");
+        }
+        return successMsg.toString();
     }
 }
