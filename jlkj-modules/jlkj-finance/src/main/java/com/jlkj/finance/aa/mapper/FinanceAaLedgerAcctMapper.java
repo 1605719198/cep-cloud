d202b6a2b4b7fbd9cffd3fc7fe12e7b4286a173b
wangmai <1481070853@qq.com>
2023-06-14 11:33:43 +0800

    修改起始日期判断条件

--- jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/aa/mapper/FinanceAaLedgerAcctMapper.java
+++ jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/aa/mapper/FinanceAaLedgerAcctMapper.java
@@ -47,6 +47,20 @@ public interface FinanceAaLedgerAcctMapper
      * @param financeAaLedgerAcctDTO 账薄-会计科目余额
      * @return 账薄-会计科目余额集合
      */
+    public List<FinanceAaLedgerAcctDTO>   selectAcctCode(FinanceAaLedgerAcctDTO financeAaLedgerAcctDTO);
+    /**
+     * 查询账薄-会计科目余额列表
+     *
+     * @param financeAaLedgerAcctDTO 账薄-会计科目余额
+     * @return 账薄-会计科目余额集合
+     */
+    public FinanceAaLedgerAcctDTO selectBgnAmtAndBgnQty(FinanceAaLedgerAcctDTO financeAaLedgerAcctDTO);
+    /**
+     * 查询账薄-会计科目余额列表
+     *
+     * @param financeAaLedgerAcctDTO 账薄-会计科目余额
+     * @return 账薄-会计科目余额集合
+     */
     public List<FinanceAaLedgerAcctDTO> selectFinanceAaGeneralLedgerAcctCode1(FinanceAaLedgerAcctDTO financeAaLedgerAcctDTO);
     /**
      * 查询账薄-会计科目余额列表
