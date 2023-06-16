1dbbef865a8bb4a4b1be4c0bb5b5f977e2c5a3e7
114288 <30182494@qq.com>
2023-06-16 13:23:35 +0800

    修改代码规范

--- jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/aa/service/impl/FinanceAaAccountBalanceTonSteelServiceImpl.java
+++ jlkj-modules/jlkj-finance/src/main/java/com/jlkj/finance/aa/service/impl/FinanceAaAccountBalanceTonSteelServiceImpl.java
@@ -10,6 +10,7 @@ import com.jlkj.finance.aa.service.IFinanceAaAccountBalanceTonSteelService;
 import com.jlkj.finance.utils.ConstantsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.jlkj.finance.utils.AssertUtil;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -58,14 +59,14 @@ public class FinanceAaAccountBalanceTonSteelServiceImpl implements IFinanceAaAcc
         }
         // 余额为零且无发生额不显示 Y
         if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getBalanceZero())){
-            financeAaLedgerAcctDTOS = selectListAdd(financeAaLedgerAcctDTOS);
+            financeAaLedgerAcctDTOS=  selectListAdd(financeAaLedgerAcctDTOS);
         }
 
         //合并层级
         if (financeAaLedgerAcctDTOS.size()>0){
-           // financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
+            financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
         }
-        return financeAaLedgerAcctDTOS;
+        return financeAaLedgerAcctDTOS1;
     }
 
     @Override
@@ -81,7 +82,6 @@ public class FinanceAaAccountBalanceTonSteelServiceImpl implements IFinanceAaAcc
      */
     @Override
     public List<FinanceAaLedgerAcctDTO> selectListCalNumberDetailIfSteel(FinanceAaLedgerAcctDTO financeAaLedgerAcctDTO) {
-
         Date dateStart = DateUtils.dateTime(DateUtils.YYYY_MM,financeAaLedgerAcctDTO.getStartDate());
         String startDate =financeAaLedgerAcctDTO.getStartDate().substring(0,7)+"-01";
         Date dateEnd = DateUtils.dateTime(DateUtils.YYYY_MM,financeAaLedgerAcctDTO.getEndDate());
@@ -93,77 +93,22 @@ public class FinanceAaAccountBalanceTonSteelServiceImpl implements IFinanceAaAcc
         List<FinanceAaLedgerAcctDTO> financeAaLedgerAcctDTOS1 = new ArrayList<>();
         List<FinanceAaLedgerAcctDTO> financeAaLedgerAcctDTOS  = new ArrayList<>();
         financeAaLedgerAcctDTO.getUnpostedVoucher();
+        //financeAaLedgerAcctDTOS = financeAaLedgerAcctMapper.selectAmountNotDisplayed(financeAaLedgerAcctDTO);
         financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedCal(financeAaLedgerAcctDTO);
+        //无发生额不显示 Y
+        if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getAmountNotDisplayed())){
+            financeAaLedgerAcctDTOS = selectList(financeAaLedgerAcctDTOS);
+        }
+        // 余额为零且无发生额不显示 Y
+        if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getBalanceZero())){
+            financeAaLedgerAcctDTOS=  selectListAdd(financeAaLedgerAcctDTOS);
+        }
 
-//
-//        if (ConstantsUtil.TYPE2.equals(financeAaLedgerAcctDTO.getReportType())){
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getAmountNotDisplayed())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedA(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectList(financeAaLedgerAcctDTOS);
-//            }
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getBalanceZero())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedA(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectListAdd(financeAaLedgerAcctDTOS);
-//            }
-//            {
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedA(financeAaLedgerAcctDTO);
-//            }
-//            if (financeAaLedgerAcctDTOS.size()>0){
-//                financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
-//            }
-//        }
-//        if (ConstantsUtil.TYPE3.equals(financeAaLedgerAcctDTO.getReportType())){
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getAmountNotDisplayed())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedB(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectList(financeAaLedgerAcctDTOS);
-//
-//            }
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getBalanceZero())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedB(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectListAdd(financeAaLedgerAcctDTOS);
-//            }
-//            {
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedB(financeAaLedgerAcctDTO);
-//            }
-//            if (financeAaLedgerAcctDTOS.size()>0){
-//                financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
-//            }
-//        }
-//        if (ConstantsUtil.TYPE4.equals(financeAaLedgerAcctDTO.getReportType())){
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getAmountNotDisplayed())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedC(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectList(financeAaLedgerAcctDTOS);
-//
-//            }
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getBalanceZero())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedC(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectListAdd(financeAaLedgerAcctDTOS);
-//            }
-//            {
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedC(financeAaLedgerAcctDTO);
-//            }
-//            if (financeAaLedgerAcctDTOS.size()>0){
-//                financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
-//            }
-//        }
-//        if (ConstantsUtil.TYPE5.equals(financeAaLedgerAcctDTO.getReportType())){
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getAmountNotDisplayed())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedD(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectList(financeAaLedgerAcctDTOS);
-//
-//            }
-//            if (ConstantsUtil.DISABLEDCODE.equals(financeAaLedgerAcctDTO.getBalanceZero())){
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedD(financeAaLedgerAcctDTO);
-//                financeAaLedgerAcctDTOS1 = selectListAdd(financeAaLedgerAcctDTOS);
-//            }
-//            {
-//                financeAaLedgerAcctDTOS = financeAaLedgerlCalMapper.selectAmountNotDisplayedD(financeAaLedgerAcctDTO);
-//            }
-//            if (financeAaLedgerAcctDTOS.size()>0){
-//                financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
-//            }
-//        }
-        return financeAaLedgerAcctDTOS;
+        //合并层级
+        if (financeAaLedgerAcctDTOS.size()>0){
+            financeAaLedgerAcctDTOS1 = selectAccountLevel(financeAaLedgerAcctDTOS, financeAaLedgerAcctDTO);
+        }
+        return financeAaLedgerAcctDTOS1;
     }
 
     @Override
@@ -175,23 +120,31 @@ public class FinanceAaAccountBalanceTonSteelServiceImpl implements IFinanceAaAcc
 
         List<FinanceAaLedgerAcctDTO> financeAaLedgerAcctDTOS = new ArrayList<>();
         for (FinanceAaLedgerAcctDTO financeAaLedgerAcctDTO1 :financeAaLedgerAcctDTO){
-            if (financeAaLedgerAcctDTO1.getDrAmt() != null && financeAaLedgerAcctDTO1.getCrAmt() != null) {
-                if (financeAaLedgerAcctDTO1.getDrAmt().compareTo(BigDecimal.ZERO) != 0 || financeAaLedgerAcctDTO1.getCrAmt().compareTo(BigDecimal.ZERO) != 0) {
-                    financeAaLedgerAcctDTOS.add(financeAaLedgerAcctDTO1);
-                }
-            }
+            if( AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getDrAmt()) .compareTo(BigDecimal.ZERO) == 0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getCrAmt()) .compareTo(BigDecimal.ZERO)  ==0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getDrQty()) .compareTo(BigDecimal.ZERO) == 0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getCrQty()) .compareTo(BigDecimal.ZERO)  ==0
+            ){
         }
+
+    }
         return financeAaLedgerAcctDTOS;
     }
 
     public List<FinanceAaLedgerAcctDTO> selectListAdd(List<FinanceAaLedgerAcctDTO> financeAaLedgerAcctDTO) {
-        List<FinanceAaLedgerAcctDTO> financeAaLedgerAcctDTOS = new ArrayList<>();
+       List<FinanceAaLedgerAcctDTO> financeAaLedgerAcctDTOS = new ArrayList<>();
         for (FinanceAaLedgerAcctDTO financeAaLedgerAcctDTO1 :financeAaLedgerAcctDTO){
-            if (financeAaLedgerAcctDTO1.getDrAmt() != null && financeAaLedgerAcctDTO1.getCrAmt() != null && financeAaLedgerAcctDTO1.getBgnAmt() != null) {
-                if (financeAaLedgerAcctDTO1.getDrAmt().compareTo(BigDecimal.ZERO) != 0 || financeAaLedgerAcctDTO1.getCrAmt().compareTo(BigDecimal.ZERO) != 0 || financeAaLedgerAcctDTO1.getBgnAmt().compareTo(BigDecimal.ZERO) != 0) {
-                    financeAaLedgerAcctDTOS.add(financeAaLedgerAcctDTO1);
-                }
+
+            if( AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getDrAmt()) .compareTo(BigDecimal.ZERO) == 0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getCrAmt()) .compareTo(BigDecimal.ZERO)  ==0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getBgnAmt()) .compareTo(BigDecimal.ZERO) == 0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getDrQty()) .compareTo(BigDecimal.ZERO) == 0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getCrQty()) .compareTo(BigDecimal.ZERO)  ==0
+                    || AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getBgnQty()) .compareTo(BigDecimal.ZERO)  == 0
+        ){
+                financeAaLedgerAcctDTOS.add(financeAaLedgerAcctDTO1);
             }
+
         }
         return financeAaLedgerAcctDTOS;
 
@@ -223,10 +176,12 @@ public class FinanceAaAccountBalanceTonSteelServiceImpl implements IFinanceAaAcc
             }else {
                 financeAaLedgerAcctDTO1.setBgnQtyBurden(financeAaLedgerAcctDTO1.getBgnAmt().negate());
             }
-            BigDecimal yearCrAmt = (null == financeAaLedgerAcctDTO1.getCrAmt() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getCrAmt()).add(null == financeAaLedgerAcctDTO1.getYearCrAmt() ? BigDecimal.ZERO : financeAaLedgerAcctDTO1.getYearCrAmt());
-            BigDecimal yearCrQty = (null == financeAaLedgerAcctDTO1.getYearCrQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getYearCrQty()).add(null == financeAaLedgerAcctDTO1.getDrQty() ? BigDecimal.ZERO : financeAaLedgerAcctDTO1.getDrQty());
-            BigDecimal yearDrAmt = (null == financeAaLedgerAcctDTO1.getYearDrAmt() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getYearDrAmt()).add(null == financeAaLedgerAcctDTO1.getDrAmt() ? BigDecimal.ZERO : financeAaLedgerAcctDTO1.getDrAmt());
-            BigDecimal yearDrQty = (null == financeAaLedgerAcctDTO1.getYearDrQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getYearDrQty()).add(null == financeAaLedgerAcctDTO1.getDrQty() ? BigDecimal.ZERO : financeAaLedgerAcctDTO1.getDrQty());
+
+            ;
+            BigDecimal yearCrAmt = AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getYearCrAmt() );
+            BigDecimal yearCrQty = AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getYearCrQty() );
+            BigDecimal yearDrAmt = AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getYearDrAmt() );;
+            BigDecimal yearDrQty =AssertUtil.bigDecimalValue(financeAaLedgerAcctDTO1.getYearDrQty() );;
             if(!yearCrAmt.equals(BigDecimal.ZERO)){
                 financeAaLedgerAcctDTO1.setYearCrAmt(yearCrAmt);
             }
@@ -241,7 +196,10 @@ public class FinanceAaAccountBalanceTonSteelServiceImpl implements IFinanceAaAcc
                 financeAaLedgerAcctDTO1.setYearDrQty(yearDrQty);
             }
 
-            BigDecimal subtract = ((null == financeAaLedgerAcctDTO1.getBgnQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getBgnQty()).add(null == financeAaLedgerAcctDTO1.getDrQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getDrQty())).subtract(null == financeAaLedgerAcctDTO1.getCrQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getCrQty());
+            BigDecimal subtract = ((null == financeAaLedgerAcctDTO1.getBgnQty() ?
+                    BigDecimal.ZERO :financeAaLedgerAcctDTO1.getBgnQty())
+                    .add(null == financeAaLedgerAcctDTO1.getDrQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getDrQty()))
+                    .subtract(null == financeAaLedgerAcctDTO1.getCrQty() ?BigDecimal.ZERO :financeAaLedgerAcctDTO1.getCrQty());
             if (subtract.compareTo(BigDecimal.ZERO) >=0){
                 financeAaLedgerAcctDTO1.setEndingBalanceQtyDr(subtract);
             }else {
