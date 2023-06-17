dde60e6d24cb659623ea5d677642e26d852a9ea5
zhangyouxin <zhangyouxin@ejianlong.com>
2023-06-17 14:30:35 +0800

    借支申请前端

---
+++ jlkj-cloud-ui/src/api/finance/ao/loanApply.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询借支申请主档列表
+export function listLoanApply(query) {
+  return request({
+    url: '/finance/loanApply/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询借支申请主档详细
+export function getLoanApply(id) {
+  return request({
+    url: '/finance/loanApply/' + id,
+    method: 'get'
+  })
+}
+
+// 新增借支申请主档
+export function addLoanApply(data) {
+  return request({
+    url: '/finance/loanApply',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改借支申请主档
+export function updateLoanApply(data) {
+  return request({
+    url: '/finance/loanApply',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除借支申请主档
+export function delLoanApply(id) {
+  return request({
+    url: '/finance/loanApply/' + id,
+    method: 'delete'
+  })
+}
