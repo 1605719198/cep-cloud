32d493ff99368dd18c7b17b21b022c3d2b081f47
jiangbingchen <114288>
2023-06-25 11:46:53 +0800

    出差统计作业

--- jlkj-cloud-ui/src/views/human/st/travelStatistics.vue
+++ jlkj-cloud-ui/src/views/human/st/travelStatistics.vue
@@ -11,9 +11,9 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="日期区间" prop="norOndutyBegin">
+      <el-form-item label="日期区间" prop="startDate">
         <el-date-picker
-          v-model="queryParams.norOndutyBegin"
+          v-model="queryParams.startDate"
           value-format="yyyy-MM-dd"
           type="daterange"
           range-separator="~"
@@ -23,10 +23,10 @@
         >
         </el-date-picker>
       </el-form-item>
-      <el-form-item label="处理情况" prop="disposeId">
-        <el-select v-model="queryParams.disposeId">
+      <el-form-item label="审批状态" prop="status">
+        <el-select v-model="queryParams.status">
           <el-option
-            v-for="dict in attendenceOptions.DisposeStatus"
+            v-for="dict in attendenceOptions.FlowStatus"
             :key="dict.dicNo"
             :label="dict.dicName"
             :value="dict.dicNo"
@@ -41,33 +41,47 @@
           icon="el-icon-download"
           size="mini"
           @click="handleExport"
-          v-hasPermi="['human:attendanceAbnormalStatistics:export']"
+          v-hasPermi="['human:travelStatistics:export']"
         >导出</el-button>
       </el-form-item>
     </el-form>
 
-    <el-table v-loading="loading" :data="attendanceAbnormalStatisticsList" >
+    <el-table v-loading="loading" :data="travelStatisticsList" >
       <el-table-column label="序号" align="center" prop="id" width="60"/>
-      <el-table-column label="姓名" align="center" prop="empName" />
       <el-table-column label="工号" align="center" prop="empNo" />
-      <el-table-column label="岗位名称" align="center" prop="postName" />
-      <el-table-column label="生产异常原因" align="center" prop="excReaId" >
+      <el-table-column label="姓名" align="center" prop="empName" />
+      <el-table-column label="部门名称" align="center" prop="deptName" />
+      <el-table-column label="出差类别" align="center" prop="travelTpye" >
         <template v-slot="scope">
-          <dict-tag-human :options="attendenceOptions.AbnormalReason" :value="scope.row.excReaId"/>
+          <dict-tag-human :options="attendenceOptions.TravelType" :value="scope.row.travelTpye"/>
         </template>
       </el-table-column>
-      <el-table-column label="出勤证明原因细分" align="center" prop="proveReason" >
-        <template v-slot="scope">
-          <dict-tag-human :options="attendenceOptions.ProveReason" :value="scope.row.proveReason"/>
+      <el-table-column label="出差开始日期" align="center" prop="startDate" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.startDate, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="正常出勤时段开始" align="center" prop="norOndutyBegin" />
-      <el-table-column label="正常出勤时段结束" align="center" prop="norOndutyEnd" />
-      <el-table-column label="实际出勤时段开始" align="center" prop="slotCardOnduty" />
-      <el-table-column label="实际出勤时段结束" align="center" prop="slotCardOffduty" />
-      <el-table-column label="处理情况" align="center" prop="disposeId" >
+      <el-table-column label="出差结束日期" align="center" prop="endDate" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.endDate, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="出差地点" align="center" prop="travelAbroad" >
         <template v-slot="scope">
-          <dict-tag-human :options="attendenceOptions.DisposeStatus" :value="scope.row.disposeId"/>
+          <dict-tag-human :options="attendenceOptions.TravelAbroad" :value="scope.row.travelAbroad"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="成本中心" align="center" prop="costCenter" />
+      <el-table-column label="出差事由" align="center" prop="travelReason" >
+        <template v-slot="scope">
+          <dict-tag-human :options="attendenceOptions.TravelReason" :value="scope.row.travelReason"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="出差单号" align="center" prop="travelNo" />
+      <el-table-column label="报支单号" align="center" prop="billNo" />
+      <el-table-column label="申请单状态" align="center" prop="status" >
+        <template v-slot="scope">
+          <dict-tag-human :options="attendenceOptions.FlowStatus" :value="scope.row.status"/>
         </template>
       </el-table-column>
     </el-table>
@@ -83,14 +97,13 @@
 </template>
 
 <script>
-import { listAttendanceAbnormalStatistics, } from "@/api/human/st/attendanceAbnormalStatistics";
+import { listTravelStatistics } from "@/api/human/st/travelStatistics";
 import {selectCompany} from "@/api/human/hp/deptMaintenance";
 import {getAttendenceOptions} from "@/api/human/hd/attendenceBasis";
 import DictTagHuman from "@/views/components/human/dictTag/humanBaseInfo";
-import {isEmpty} from "element-ui";
 
 export default {
-  name: "AttendanceAbnormalStatistics",
+  name: "TravelStatistics",
   components: { DictTagHuman },
   data() {
     return {
@@ -101,7 +114,7 @@ export default {
       //绩效选单类型查询
       attendenceOptionType:{
         id:'',
-        optionsType:['DisposeStatus','AbnormalReason','ProveReason']
+        optionsType:['FlowStatus','TravelType','TravelReason','TravelAbroad']
       },
       // 遮罩层
       loading: false,
@@ -115,8 +128,8 @@ export default {
       showSearch: true,
       // 总条数
       total: 0,
-      // 出勤异常统计表格数据
-      attendanceAbnormalStatisticsList: [],
+      // 出差统计表格数据
+      travelStatisticsList: [],
       // 弹出层标题
       title: "",
       // 是否显示弹出层
@@ -126,8 +139,8 @@ export default {
         pageNum: 1,
         pageSize: 10,
         compId: this.$store.state.user.userInfo.compId,
-        disposeId: null,
-        norOndutyBegin: null,
+        status: null,
+        startDate: null,
         date1:null,
         date2:null,
       },
@@ -150,65 +163,68 @@ export default {
         }
       )
     },
-    //查询绩效选单
-    getDisc(){
-      getAttendenceOptions(this.attendenceOptionType).then(response=> {
-        this.attendenceOptions=response.data;
-      })
+    /** 查询出差统计列表 */
+    getList() {
+      this.loading = true;
+      listTravelStatistics(this.queryParams).then(response => {
+        this.travelStatisticsList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
     },
     /** 日期查询范围变更*/
     dateFormat(val){
       if(val==null){
         this.queryParams.date1=null
         this.queryParams.date2=null
-        this.queryParams.norOndutyBegin=null
+        this.queryParams.startDate=null
         return
       }
       this.queryParams.date1 =val[0];
       this.queryParams.date2 =val[1];
     },
-    /** 查询出勤异常统计列表 */
-    getList() {
-      this.loading = true;
-      listAttendanceAbnormalStatistics(this.queryParams).then(response => {
-        this.attendanceAbnormalStatisticsList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
+    //查询绩效选单
+    getDisc(){
+      getAttendenceOptions(this.attendenceOptionType).then(response=> {
+        this.attendenceOptions=response.data;
+      })
     },
     // 表单重置
     reset() {
       this.form = {
         id: null,
+        travelNo: null,
+        travelTpye: null,
+        travelReason: null,
         compId: null,
         empId: null,
-        empName: null,
         empNo: null,
-        postId: null,
-        postName: null,
-        turnTypeId: null,
-        turnTypeName: null,
-        classId: null,
-        className: null,
-        excReaId: null,
-        proveReaId: null,
-        proveReason: null,
+        empName: null,
+        deptName: null,
         status: null,
-        norOndutyBegin: null,
-        norOndutyEnd: null,
-        slotCardOnduty: null,
-        slotCardOffduty: null,
-        description: null,
-        chiefempnoId: null,
-        chiefempnoNo: null,
-        chiefempnoName: null,
-        chiefempnoPostId: null,
-        chiefempnoPostname: null,
-        disposeId: null,
-        auditType: null,
+        agentId: null,
+        agentNo: null,
+        agentName: null,
+        costCenter: null,
+        contract: null,
+        startDate: null,
+        startTime: null,
+        endDate: null,
+        endTime: null,
+        realStartDate: null,
+        realStartTime: null,
+        realEndDate: null,
+        realEndTime: null,
+        reasonDesc: null,
+        billNo: null,
+        isEngineer: null,
         creator: null,
         creatorId: null,
-        createDate: null
+        createDate: null,
+        resvAttr1: null,
+        resvAttr2: null,
+        resvAttr3: null,
+        resvAttr4: null
       };
       this.resetForm("form");
     },
@@ -219,9 +235,9 @@ export default {
     },
     /** 导出按钮操作 */
     handleExport() {
-      this.download('human/attendanceAbnormalStatistics/export', {
+      this.download('human/travelStatistics/export', {
         ...this.queryParams
-      }, `attendanceAbnormalStatistics_${new Date().getTime()}.xlsx`)
+      }, `travelStatistics_${new Date().getTime()}.xlsx`)
     }
   }
 };
---
+++ jlkj-modules/jlkj-human/src/main/java/com/jlkj/human/st/domain/TravelStatistics.java
@@ -0,0 +1,508 @@
+package com.jlkj.human.st.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jlkj.common.core.annotation.Excel;
+import com.jlkj.common.core.web.domain.BaseEntity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.Date;
+
+/**
+ * 出差统计对象 human_hd_travel_application
+ *
+ * @author 267383
+ * @date 2023-06-24
+ */
+public class TravelStatistics extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 主键 */
+    @Excel(name = "序号")
+    private String id;
+
+    /** 工号 */
+    @Excel(name = "工号")
+    private String empNo;
+
+    /** 姓名 */
+    @Excel(name = "姓名")
+    private String empName;
+
+    /** 部门名称 */
+    @Excel(name = "部门名称")
+    private String deptName;
+
+    /** 出差类别 */
+    @Excel(name = "出差类别")
+    private String travelTpye;
+
+    /** 出差事由类别 */
+    @Excel(name = "出差事由")
+    private String travelReason;
+
+    /** 公司别 */
+    private String compId;
+
+    /** 员工id */
+    private String empId;
+
+    @Excel(name = "出差地点")
+    private String travelAbroad;
+
+    /** 代理人ID */
+    private String agentId;
+
+    /** 代理人NO */
+    private String agentNo;
+
+    /** 代理人 */
+    private String agentName;
+
+    /** 成本中心 */
+    @Excel(name = "成本中心")
+    private String costCenter;
+
+    /** 预算编号 */
+    private String contract;
+
+    /** 出差开始日期 */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "出差开始日期", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date startDate;
+
+    /** 出差开始时分 */
+    private String startTime;
+
+    /** 出差结束日期 */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "出差结束日期", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date endDate;
+
+    /** 出差结束时分 */
+    private String endTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date1;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date2;
+
+    /** 实际出差开始日期 */
+    private Date realStartDate;
+
+    /** 实际出差开始时分 */
+    private String realStartTime;
+
+    /** 实际出差结束日期 */
+    private Date realEndDate;
+
+    /** 实际出差结束时分 */
+    private String realEndTime;
+
+    /** 出差事由 */
+    private String reasonDesc;
+
+    /** 出差单号 */
+    @Excel(name = "出差单号")
+    private String travelNo;
+
+    /** 报支单号 */
+    @Excel(name = "报支单号")
+    private String billNo;
+
+    /** 申请单状态 */
+    @Excel(name = "审核状态")
+    private String status;
+
+    /** 是否工程费用 */
+    private String isEngineer;
+
+    /** 输入人 */
+    private String creator;
+
+    /** 输入人ID */
+    private String creatorId;
+
+    /** 输入日期 */
+    private Date createDate;
+
+    /** 预留属性1 */
+    private String resvAttr1;
+
+    /** 预留属性2 */
+    private String resvAttr2;
+
+    /** 预留属性3 */
+    private String resvAttr3;
+
+    /** 预留属性4 */
+    private String resvAttr4;
+
+    public void setDate1(Date date1)
+    {
+        this.date1 = date1;
+    }
+    public Date getDate1()
+    {
+        return date1;
+    }
+
+    public void setDate2(Date date2)
+    {
+        this.date2 = date2;
+    }
+    public Date getDate2()
+    {
+        return date2;
+    }
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+    public void setTravelNo(String travelNo)
+    {
+        this.travelNo = travelNo;
+    }
+
+    public String getTravelNo()
+    {
+        return travelNo;
+    }
+    public void setTravelTpye(String travelTpye)
+    {
+        this.travelTpye = travelTpye;
+    }
+
+    public String getTravelTpye()
+    {
+        return travelTpye;
+    }
+    public void setTravelReason(String travelReason)
+    {
+        this.travelReason = travelReason;
+    }
+
+    public String getTravelReason()
+    {
+        return travelReason;
+    }
+    public void setCompId(String compId)
+    {
+        this.compId = compId;
+    }
+
+    public String getCompId()
+    {
+        return compId;
+    }
+    public void setEmpId(String empId)
+    {
+        this.empId = empId;
+    }
+
+    public String getEmpId()
+    {
+        return empId;
+    }
+    public void setEmpNo(String empNo)
+    {
+        this.empNo = empNo;
+    }
+
+    public String getEmpNo()
+    {
+        return empNo;
+    }
+    public void setEmpName(String empName)
+    {
+        this.empName = empName;
+    }
+
+    public String getEmpName()
+    {
+        return empName;
+    }
+    public void setDeptName(String deptName)
+    {
+        this.deptName = deptName;
+    }
+
+    public String getDeptName()
+    {
+        return deptName;
+    }
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+    public void setAgentId(String agentId)
+    {
+        this.agentId = agentId;
+    }
+
+    public String getAgentId()
+    {
+        return agentId;
+    }
+    public void setAgentNo(String agentNo)
+    {
+        this.agentNo = agentNo;
+    }
+
+    public String getAgentNo()
+    {
+        return agentNo;
+    }
+    public void setAgentName(String agentName)
+    {
+        this.agentName = agentName;
+    }
+
+    public String getAgentName()
+    {
+        return agentName;
+    }
+    public void setCostCenter(String costCenter)
+    {
+        this.costCenter = costCenter;
+    }
+
+    public String getCostCenter()
+    {
+        return costCenter;
+    }
+    public void setContract(String contract)
+    {
+        this.contract = contract;
+    }
+
+    public String getContract()
+    {
+        return contract;
+    }
+    public void setStartDate(Date startDate)
+    {
+        this.startDate = startDate;
+    }
+
+    public Date getStartDate()
+    {
+        return startDate;
+    }
+    public void setStartTime(String startTime)
+    {
+        this.startTime = startTime;
+    }
+
+    public String getStartTime()
+    {
+        return startTime;
+    }
+    public void setEndDate(Date endDate)
+    {
+        this.endDate = endDate;
+    }
+
+    public Date getEndDate()
+    {
+        return endDate;
+    }
+    public void setEndTime(String endTime)
+    {
+        this.endTime = endTime;
+    }
+
+    public String getEndTime()
+    {
+        return endTime;
+    }
+    public void setRealStartDate(Date realStartDate)
+    {
+        this.realStartDate = realStartDate;
+    }
+
+    public Date getRealStartDate()
+    {
+        return realStartDate;
+    }
+    public void setRealStartTime(String realStartTime)
+    {
+        this.realStartTime = realStartTime;
+    }
+
+    public String getRealStartTime()
+    {
+        return realStartTime;
+    }
+    public void setRealEndDate(Date realEndDate)
+    {
+        this.realEndDate = realEndDate;
+    }
+
+    public Date getRealEndDate()
+    {
+        return realEndDate;
+    }
+    public void setRealEndTime(String realEndTime)
+    {
+        this.realEndTime = realEndTime;
+    }
+
+    public String getRealEndTime()
+    {
+        return realEndTime;
+    }
+    public void setReasonDesc(String reasonDesc)
+    {
+        this.reasonDesc = reasonDesc;
+    }
+
+    public String getReasonDesc()
+    {
+        return reasonDesc;
+    }
+    public void setBillNo(String billNo)
+    {
+        this.billNo = billNo;
+    }
+
+    public String getBillNo()
+    {
+        return billNo;
+    }
+    public void setIsEngineer(String isEngineer)
+    {
+        this.isEngineer = isEngineer;
+    }
+
+    public String getIsEngineer()
+    {
+        return isEngineer;
+    }
+    public void setCreator(String creator)
+    {
+        this.creator = creator;
+    }
+
+    public String getCreator()
+    {
+        return creator;
+    }
+    public void setCreatorId(String creatorId)
+    {
+        this.creatorId = creatorId;
+    }
+
+    public String getCreatorId()
+    {
+        return creatorId;
+    }
+    public void setCreateDate(Date createDate)
+    {
+        this.createDate = createDate;
+    }
+
+    public Date getCreateDate()
+    {
+        return createDate;
+    }
+    public void setResvAttr1(String resvAttr1)
+    {
+        this.resvAttr1 = resvAttr1;
+    }
+
+    public String getResvAttr1()
+    {
+        return resvAttr1;
+    }
+    public void setResvAttr2(String resvAttr2)
+    {
+        this.resvAttr2 = resvAttr2;
+    }
+
+    public String getResvAttr2()
+    {
+        return resvAttr2;
+    }
+    public void setResvAttr3(String resvAttr3)
+    {
+        this.resvAttr3 = resvAttr3;
+    }
+
+    public String getResvAttr3()
+    {
+        return resvAttr3;
+    }
+    public void setResvAttr4(String resvAttr4)
+    {
+        this.resvAttr4 = resvAttr4;
+    }
+
+    public String getResvAttr4()
+    {
+        return resvAttr4;
+    }
+    public void setTravelAbroad(String travelAbroad)
+    {
+        this.travelAbroad = travelAbroad;
+    }
+
+    public String getTravelAbroad()
+    {
+        return travelAbroad;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+                .append("date1", getDate1())
+                .append("date2", getDate2())
+            .append("travelNo", getTravelNo())
+                .append("travelAbroad", getTravelAbroad())
+            .append("travelTpye", getTravelTpye())
+            .append("travelReason", getTravelReason())
+            .append("compId", getCompId())
+            .append("empId", getEmpId())
+            .append("empNo", getEmpNo())
+            .append("empName", getEmpName())
+            .append("deptName", getDeptName())
+            .append("status", getStatus())
+            .append("agentId", getAgentId())
+            .append("agentNo", getAgentNo())
+            .append("agentName", getAgentName())
+            .append("costCenter", getCostCenter())
+            .append("contract", getContract())
+            .append("startDate", getStartDate())
+            .append("startTime", getStartTime())
+            .append("endDate", getEndDate())
+            .append("endTime", getEndTime())
+            .append("realStartDate", getRealStartDate())
+            .append("realStartTime", getRealStartTime())
+            .append("realEndDate", getRealEndDate())
+            .append("realEndTime", getRealEndTime())
+            .append("reasonDesc", getReasonDesc())
+            .append("billNo", getBillNo())
+            .append("isEngineer", getIsEngineer())
+            .append("creator", getCreator())
+            .append("creatorId", getCreatorId())
+            .append("createDate", getCreateDate())
+            .append("resvAttr1", getResvAttr1())
+            .append("resvAttr2", getResvAttr2())
+            .append("resvAttr3", getResvAttr3())
+            .append("resvAttr4", getResvAttr4())
+            .toString();
+    }
+}
