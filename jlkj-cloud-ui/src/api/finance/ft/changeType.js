256df43b0783f25e7eb3f44dff06931ee3e6b28d
sunxutong <muyemid@163.com>
2023-06-26 18:49:13 +0800

    固定资产变动方式

---
+++ jlkj-cloud-ui/src/api/finance/ft/changeType.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询固定资产变动方式列表
+export function listChangeType(query) {
+  return request({
+    url: '/finance/changeType/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询固定资产变动方式详细
+export function getChangeType(uuid) {
+  return request({
+    url: '/finance/changeType/' + uuid,
+    method: 'get'
+  })
+}
+
+// 新增固定资产变动方式
+export function addChangeType(data) {
+  return request({
+    url: '/finance/changeType',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改固定资产变动方式
+export function updateChangeType(data) {
+  return request({
+    url: '/finance/changeType',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除固定资产变动方式
+export function delChangeType(uuid) {
+  return request({
+    url: '/finance/changeType/' + uuid,
+    method: 'delete'
+  })
+}
