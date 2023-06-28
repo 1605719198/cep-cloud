03cdd1fbd4a7565f84d115040bf0988c92356b56
xin <1195059682@qq.com>
2023-06-28 14:57:17 +0800

    ureport配置SpringBean数据源

---
+++ jlkj-api/jlkj-api-finance/src/main/java/com/jlkj/finance/api/domain/UserVo.java
@@ -0,0 +1,14 @@
+package com.jlkj.finance.api.domain;
+
+import lombok.Data;
+
+/**
+ * @author jlkj
+ */
+@Data
+public class UserVo {
+    private String id;
+    private String userNode;
+    private String userName;
+    private String sex;
+}
