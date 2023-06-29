b6be9ddeb35beee7e58dd1b4bd99e61af0c52646
114288 <30182494@qq.com>
2023-06-29 14:55:51 +0800

    报表bean

--- jlkj-modules/jlkj-ureport/src/main/java/com/jlkj/ureport/configs/DataSourceAaBean.java
+++ jlkj-modules/jlkj-ureport/src/main/java/com/jlkj/ureport/configs/DataSourceAaBean.java
@@ -1,15 +1,18 @@
 package com.jlkj.ureport.configs;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.jlkj.common.core.constant.SecurityConstants;
 import com.jlkj.finance.api.RemoteAaApiService;
-import com.jlkj.finance.api.domain.UserVo;
+import com.jlkj.finance.api.RemoteAaReportService;
+
+import com.jlkj.finance.api.domain.FinanceAaLedgerAcctVo;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.RandomStringUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +23,8 @@ import java.util.Map;
 @Slf4j
 public class DataSourceAaBean {
     @Autowired
+    private RemoteAaReportService remoteAaReportService;
+    @Autowired
     private RemoteAaApiService remoteAaApiService;
     /**
      * 第一种：返回Map集合
@@ -28,43 +33,17 @@ public class DataSourceAaBean {
      * @param parameters
      * @return
      */
-    public List<Map<String, String>> accountBalanceTonSteelData(String dsName, String dataSetName, Map<String, Object> parameters) {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+    public   List<FinanceAaLedgerAcctVo> accountBalanceTonSteelData(String dsName, String dataSetName, Map<String, Object> parameters) {
+        log.info("接收到参数=》{}",parameters);
 
-        Map<String, Object> m1 = new HashMap<String, Object>();
-        m1.put("year", "2000");
-        m1.put("month", "1");
-        m1.put("amount", 72847);
-        list.add(m1);
+        List<Map<String, String>> maps = remoteAaReportService.accountBalanceTonSteelData(parameters,SecurityConstants.INNER);
 
-        Map<String, Object> m2 = new HashMap<String, Object>();
-        m2.put("year", "2000");
-        m2.put("month", "2");
-        m2.put("amount", 28429);
-        list.add(m2);
-        List<Map<String, String>> maps = remoteAaApiService.selectCompanyList(SecurityConstants.INNER);
-        log.info("获取到数据-》{}",maps);
-        return maps;
+        JSONArray array= JSONArray.parseArray(JSON.toJSONString(maps)) ;
+        List<FinanceAaLedgerAcctVo> entityList = JSONArray.parseArray(array.toJSONString(),FinanceAaLedgerAcctVo.class);
+    log.info("maps=》{}",maps);
+        return entityList;
     }
 
-    /**
-     * 第二种：返回对象集合
-     * @param dsName
-     * @param datasetName
-     * @param parameters
-     * @return
-     */
-    public List<UserVo> accountBalanceTonSteelTitle(String dsName, String datasetName, Map<String, Object> parameters) {
-        List<UserVo> list = new ArrayList<UserVo>();
-        for (int i = 0; i < 10; i++) {
-            UserVo m = new UserVo();
-            m.setId(String.valueOf(i));
-            m.setUserNode(RandomStringUtils.random(10, true, false));
-            m.setUserName("name-"+i);
-            m.setSex((i%2)+"");
-            list.add(m);
-        }
-        return list;
-    }
+
 
 }
