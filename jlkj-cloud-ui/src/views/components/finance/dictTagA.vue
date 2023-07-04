76fcd35c0713ca140ab344ba2203262dcf76ad0b
sunxutong <muyemid@163.com>
2023-07-04 09:36:34 +0800

    1

---
+++ jlkj-cloud-ui/src/views/components/finance/dictTagA.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <template v-for="(item, index) in options">
+      <template v-if="values.includes(item.value)&&(isId==='false')&&(idString==='false')">
+        <span
+          :key="item.value"
+          :index="index"
+        >{{ item.label }}</span
+        >
+      </template>
+      <template v-if="(isId==='true')&&(idString==='false')&&values.includes(item.id.toString())">
+        <span
+          :key="item.id"
+          :index="index"
+        >{{ item.label }}</span
+        >
+      </template>
+      <template v-if="values.includes(item.uuid)&&(idString==='true')&&(isId==='false')">
+        <span
+          :key="item.value"
+          :index="index"
+        >{{ item.label }}</span
+        >
+      </template>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DictTag",
+  props: {
+    options: {
+      type: Array,
+      default: null,
+    },
+    value: [Number, String, Array],
+    isId: {
+      type: String,
+      default: 'false'
+    },
+    idString:{
+      type: String,
+      default: 'false'
+    }
+  },
+  computed: {
+    values() {
+      if (this.value !== null && typeof this.value !== 'undefined') {
+        return Array.isArray(this.value) ? this.value : [String(this.value)];
+      } else {
+        return [];
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+</style>
