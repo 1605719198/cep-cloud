bada7bf14a42e86c702239778800434154a0d0cb
266861 <arrogant203@163.com>
2023-07-06 14:10:06 +0800

    组织机构接口权限调整

--- jlkj-modules/jlkj-human/src/main/java/com/jlkj/human/hp/controller/HumanDeptController.java
+++ jlkj-modules/jlkj-human/src/main/java/com/jlkj/human/hp/controller/HumanDeptController.java
@@ -47,7 +47,6 @@ public class HumanDeptController extends BaseController
     * @author 266861
     * @date 2023/6/21 14:59
     **/
-    @RequiresPermissions("human:deptMaintenance:list")
     @GetMapping("/list")
     public TableDataInfo list(HumanDept humanDept)
     {
